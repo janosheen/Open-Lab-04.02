21d4d78212b8c1ab043849c8c989f456195660fa
janosheen <91180541+janosheen@users.noreply.github.com>
2021-11-09 17:22:04 +0100

    I implemented the method that creates a string with every letter doubled.

--- Open-Lab-04.02/Doubler.cs
+++ Open-Lab-04.02/Doubler.cs
@@ -6,7 +6,12 @@ namespace Open_Lab_04._02
     {
         public string DoubleChar(string original)
         {
-            throw new NotImplementedException();
+            string letter = "";
+            for (int i = 0; i < original.Length; i++)
+                {
+                letter = letter + original[i] + original[i];
+                }
+            return letter;
         }
     }
 }
